8dbf96ac42a13097d9ab7e880bec32ae0eaa7eda
paulbrowne <github@firstpartners.net>
2020-03-18 21:22:03 +0000

    Tests passing

--- test/dao/Loader_test.py
+++ test/dao/Loader_test.py
@@ -20,6 +20,7 @@ class Document_loader_test (unittest.TestCase):
         self.assertIsNotNone(num_result)
         self.assertGreaterEqual(num_result,0)
 
+
     def test_add_document(self):
 
         docLoader = scripts.dao.Loader.DocumentLoader(self.pre_config)
@@ -33,16 +34,21 @@ class Document_loader_test (unittest.TestCase):
     def test_add_remove_document_node(self):
 
         docLoader = scripts.dao.Loader.DocumentLoader(self.pre_config)
-
+        num_result_before = docLoader.count_nodes()
+        
         myDoc = scripts.model.DocumentNode.DocumentNode()
         myDoc.filename="Another node-y filename.txt"
         myDoc.text="blah blah blah blah blah blah blah blah blah blah blah blah"
         myDoc.testdata=True
 
-        num_result_before = docLoader.count_nodes()
+        
         docLoader.add_node(myDoc)
         num_result_after = docLoader.count_nodes()
         self.assertEqual(num_result_before+1,num_result_after)
+        
+        docLoader.remove_node(myDoc)
+        num_result_after_after = docLoader.count_nodes()
+        self.assertEqual(num_result_before,num_result_after_after)
         
         
         
